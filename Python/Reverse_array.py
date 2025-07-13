dc546a2c2814a384cb4674208dfada6352b8f245
SujasriMuvvala <muvvalasujasri6666@gmail.com>
2025-07-13 13:47:47 +0530

    Added Reverse_array problem

---
+++ Python/Reverse_array.py
@@ -0,0 +1,11 @@
+# 🔁 Reverse an array using Python
+
+def reverse_array(arr):
+    return arr[::-1]  # Slicing method
+
+# Example usage
+arr = [1, 2, 3, 4, 5]
+print("Original array:", arr)
+
+reversed_arr = reverse_array(arr)
+print("Reversed array:", reversed_arr)
