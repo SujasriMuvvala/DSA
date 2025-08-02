b61085ffeaa5f7264876c5396513ab4bea212876
SujasriMuvvala <muvvalasujasri6666@gmail.com>
2025-08-02 19:11:41 +0530

    Added string palindrome code

---
+++ Python/Palindrome.py
@@ -0,0 +1,17 @@
+def is_palindrome(s):
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
+# Example usage
+input_str = "madam"
+if is_palindrome(input_str):
+    print("Palindrome")
+else:
+    print("Not a palindrome")
