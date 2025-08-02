22d374a18cdf6662bc7858a32486260d147cb1b2
SujasriMuvvala <muvvalasujasri6666@gmail.com>
2025-08-02 19:05:57 +0530

    Added Reverse string code

---
+++ C/reverse_string.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char str[]) {
+    int left = 0;
+    int right = strlen(str) - 1;
+    char temp;
+
+    while (left < right) {
+        // Swap characters
+        temp = str[left];
+        str[left] = str[right];
+        str[right] = temp;
+
+        left++;
+        right--;
+    }
+}
+
+int main() {
+    char str[] = "hello";
+    reverseString(str);
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
+
